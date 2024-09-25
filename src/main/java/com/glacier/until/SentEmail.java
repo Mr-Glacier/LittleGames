50bc00e94ad7c5beac0ca497df26a1f64acd4e6a
Mr-Glacier <K15100415112@163.com>
2024-09-25 23:50:57 +0800

    add SendEmail until

---
+++ src/main/java/com/glacier/until/SentEmail.java
@@ -0,0 +1,113 @@
+package com.glacier.until;
+
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * @author Mr-Glacier
+ * @version 1.0
+ * @apiNote 邮件发送工具类 JavaMail 版本: 1.6.0 JDK 版本: JDK 1.7 以上（必须）
+ * @since 2024/9/25 23:00
+ */
+public class SentEmail {
+
+    public static String myEmailAccount = "";
+    public static String myEmailPassword = "";
+    public static String myEmailSMTPHost = "";
+    public static String receiveMailAccountDafult = "";
+
+
+    public static void methodSentEmail(String messageEmail, String subject, String receiveMailAccount) {
+        try {
+            // 1. 创建参数配置, 用于连接邮件服务器的参数配置 , 使用的协议（JavaMail规范要求）,发件人的邮箱的 SMTP 服务器地址,需要请求认证
+            Properties props = new Properties();
+            props.setProperty("mail.transport.protocol", "smtp");
+            props.setProperty("mail.smtp.host", myEmailSMTPHost);
+            props.setProperty("mail.smtp.auth", "true");
+            // 2. 根据配置创建会话对象, 用于和邮件服务器交互
+            Session session = Session.getInstance(props);
+            // 设置为debug模式, 可以查看详细的发送 log, 但是调试好之后直接false就可以
+            session.setDebug(false);
+            // 3. 创建一封邮件
+            if (receiveMailAccount == null) {
+                receiveMailAccount = receiveMailAccountDafult;
+            }
+            MimeMessage message = createMimeMessage(session, myEmailAccount, receiveMailAccount, messageEmail, subject);
+
+            // 4. 根据 Session 获取邮件传输对象
+            Transport transport = session.getTransport();
+
+            // 5. 使用 邮箱账号 和 密码 连接邮件服务器, 这里认证的邮箱必须与 message 中的发件人邮箱一致, 否则报错
+            //
+            //    PS_01: 成败的判断关键在此一句, 如果连接服务器失败, 都会在控制台输出相应失败原因的 log,
+            //           仔细查看失败原因, 有些邮箱服务器会返回错误码或查看错误类型的链接, 根据给出的错误
+            //           类型到对应邮件服务器的帮助网站上查看具体失败原因。
+            //
+            //    PS_02: 连接失败的原因通常为以下几点, 仔细检查代码:
+            //           (1) 邮箱没有开启 SMTP 服务;
+            //           (2) 邮箱密码错误, 例如某些邮箱开启了独立密码;
+            //           (3) 邮箱服务器要求必须要使用 SSL 安全连接;
+            //           (4) 请求过于频繁或其他原因, 被邮件服务器拒绝服务;
+            //           (5) 如果以上几点都确定无误, 到邮件服务器网站查找帮助。
+            //
+            //    PS_03: 仔细看log, 认真看log, 看懂log, 错误原因都在log已说明。
+            transport.connect(myEmailAccount, myEmailPassword);
+
+            // 6. 发送邮件, 发到所有的收件地址, message.getAllRecipients() 获取到的是在创建邮件对象时添加的所有收件人, 抄送人, 密送人
+            transport.sendMessage(message, message.getAllRecipients());
+
+            // 7. 关闭连接
+            transport.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 创建一封只包含文本的简单邮件
+     *
+     * @param session     和服务器交互的会话
+     * @param sendMail    发件人邮箱
+     * @param receiveMail 收件人邮箱
+     * @return
+     * @throws Exception
+     */
+    public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail, String messageEmail, String subjectTitle) throws Exception {
+        // this object contains the current date value
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String time = formatter.format(date);
+
+        // 1. 创建一封邮件
+        MimeMessage message = new MimeMessage(session);
+
+        // 2. From: 发件人（昵称有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改昵称）
+        message.setFrom(new InternetAddress(sendMail, "Mr-Glacier", "UTF-8"));
+
+        // 3. To: 收件人（可以增加多个收件人、抄送、密送）
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiveMail, "Mr-Glacier-listen", "UTF-8"));
+
+        // 4. Subject: 邮件主题（标题有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改标题）
+        message.setSubject(subjectTitle, "UTF-8");
+
+        // 5. Content: 邮件正文（可以使用html标签）（内容有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改发送内容）
+        message.setContent(messageEmail, "text/html;charset=UTF-8");
+
+        // 6. 设置发件时间
+        message.setSentDate(new Date());
+
+        // 7. 保存设置
+        message.saveChanges();
+
+        return message;
+    }
+
+}
+
